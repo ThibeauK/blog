85f912d21c94d4fab6aa8c0bce120895af1f7f54
ThibeauK <123963805+ThibeauK@users.noreply.github.com>
2026-03-12 10:47:58 +0100

    toevoegen nieuwe boeken & info

--- js/index.js
+++ js/index.js
@@ -455,7 +455,74 @@ async function init() {
       if (lightbox && lightbox.classList) lightbox.classList.remove("open");
       if (container && container.classList) container.classList.remove("close");
       if (allcontainers && allcontainers.classList) allcontainers.classList.remove("close");
-      if (container) container.innerHTML = `<div class="info">Contact info and other details here.</div>`;
+      if (container) container.innerHTML = `
+      <div class="info">
+          <p>Tom D’haenens (born 1969) is a Belgian artistic photographer and publisher of unique photo books. He received his photography education at the Ghent Academy of Arts. Later he moved to Baltimore (US) where he deeply inhaled the business life of the Metropolis of New York. He soon made quite a name in the international business world as a photographic artist with that special eye that makes the difference. 
+In this multinational environment he has always been fascinated by the exterior signs of economic and societal globalization. He has travelled to about every corner of the world, always on the lookout for that special unique shot that will capture the essence of a moment in time. In doing so he translates this world in terms of beauty and pathos. Nothing is dull, nothing is monotonous. Recognizing the rhetorical power of photography, D’haenens freezes his subject with a distinguished lightness. He possesses the ability to convert banal reality as much as unique human achievement into a brilliant image. In a split second, a detail of framed reality is given an unprecedented lustre that amazes his public. The unnoticed is given visibility that strikes us, catches us, teases us, triggers our curiosity and that challenges us to get a closer look into the story behind the picture. 
+Tom D’haenens’ pictures have immortalized the most prestigious projects of a large number of multinationals in a number of amazing photo books, including the prize-winning ‘Brussels Airport’, ‘The Port of Antwerp’, ‘Flanders Port Area’, ‘Creating Land for the Future, ‘Ghent, so much city’, ‘Flanders State of the Arts’ and many more.
+He is an expert in bringing together a panoply of the most diverse elements into original concepts, with a unique eye for colour, light, contrast and detail: his absolute trademark today.</p>
+
+          <p>Tom D'haenens (1969) has been photographing at home and abroad for more than 30 years. He has been commissioned by multinationals and international governments. In his twenties he completed his education at the Academy of Fine Arts in Ghent and moved to Baltimore, USA to study the art of photography. In New York he quickly made a name for himself. He has photographed some of the most prestigious projects for a large number of multinationals. He later returned to his first love: industry. As a result, industry and architecture have become his playground. He has become an expert in bringing both elements together in highly original concepts with an eye for colour and detail. In fact, they have become his trademark.
+Tom portrays the economic portals of countries and the industry within them in his own individual way. His work is a unique combination of technical precision and artistic vision. His artful approach to industry has made him a global pioneer in his field.
+You may have seen something 1000 times, yet Tom shows that the 1001st time can still make a difference. That difference is in an unusual angle, a different light, a chance moment. The curiosity about that one time when an image rises above banality is what keeps him restless, even now, after thirty years of photography and seeing the world. The photographs imply that there is still more, before and after, both in space and in time. Anyone who looks at them will automatically add to them. That is how it is intended to be. His photos don't stop at the margin. He loves nothing more than to stimulate our curiosity and feed our imagination. He challenges us to make our own interpretation in his photographs and to discover something different each time. Hence his urge to collect photos in books. Photo books as viewing books, search books, discovery books.
+          </p>
+
+          <div id="quote">
+          <blockquote>
+          Tom D’haenens mag zich als fotograaf terecht het epitheton peintre de la vie postmoderne toe-eigenen. D’haenens illustreert naast de thema’s die tegenwoordig in stedenbouwkundige theorieën centraal staan ook de maatschappelijke en economische facetten van de globalisering. Grote kantoorgebouwen, atria, monoculturele woonwijken, technologische bedrijven, shopping malls, luchthavens, hotellobby’s kortom de nieuwe spaces of flow worden door deze jonge fotograaf in heldere kleuren en zonder complexen neergezet.  In zijn werk wordt deze gefotografeerde wereld vertaald in termen van schoonheid en pathos. D’haenens is ruimschoots vertrouwd met zowel de geschiedenis van de fotografie als haar retorisch vermogen. Wie zijn prenten bestudeert, voelt doorheen zijn kunst de trilling van coryfeeën als Ansel Adams, Margaret Bourk-White, Stieglitz, Walker Evans, Edward Weston en talloze andere goden. Jawel, zijn fotokunst wortelt in de Amerikaanse traditie van ongedwongenheid, ze bezit bovendien de gave om banale realiteit in een nieuw daglicht te zetten. En natuurlijk is er de Amerikaanse erfenis van de school van Baltimore die D’haenens met zich meedraagt. Beelden van een ingesnoerde werkelijkheid waar we doorgaans onachtzaam aan voorbijgaan worden boeiend en interessant bevonden. D’haenens geeft het postmoderne tijdvak een ongekende en verleidelijke glans die zich spiegelt in het detail. Vraag is hoe we die glans moeten lezen. Zijn beelden in se wel zo onschuldig als ze zich voordoen?  Feit is dat D’haenens een wereld voor zich heeft die in toenemende mate uit niet-plaatsen bestaat. Ze zijn vooral te vinden op het terrein van de mobiliteit en de consumptie: hotels, supermarkten, winkelcentra, pleisterplaatsen langs de snelweg en vliegvelden. Het zijn de hyperspaces van deze tijd, ze verbeelden de evacuatie van de publieke ruimte.
+          </blockquote>
+          <p id="auteur">– Philip Willaert</p>
+          </div>
+
+          <div id="tekstblok">
+          <p>I look at the world as it is
+<br>
+Besides the purely architectural aspect of buildings, Tom D'haenens also likes to photograph the social and economic facets of globalisation. Large construction projects such as office buildings, atria, monocultural residential areas, technological companies, shopping malls, airports, hotel lobbies - in short, the new spaces of flows - are portrayed by this seasoned photographer in bright colours and vibrant shapes without complexes. 
+
+In his work, the photographer translates this world in terms of beauty and pathos. D'haenens knows his photographic history; those who study his prints can feel the vibe of luminaries such as Ansel Adams, Margaret Bourk-White, Stieglitz, Walker Evans, Edward Weston and countless others. Certainly, his photographic art is rooted in the American tradition of casualness and gift for casting banal reality into a new light. 
+
+But D'haenens also carries with him the business legacy of the Baltimore School of Art. Images of a constricted reality which we usually pass by negligently become fascinating and unusually interesting. D'haenens gives the present-day world an unprecedented glow, optimism reigns in his shots, although at the same time he forces the viewer to ask questions.
+<br><br>
+Circular economy
+<br>
+D'haenens seems to see the sea as the forgotten landscape of the 20th and 21st centuries. Large transport flows traverse the various oceans of the world, and in D'haenens' oeuvre cargo ships, ports and containers play a prominent role. As a photographer, he too realises that half of all goods today are shipped in containers: but these are odourless, shapeless, anonymous. The smells of tuna, or coconut, or hemp have completely disappeared from ships and ports. The container has become the symbol of global trade, with the sea as binding agent of all continents. With ultrasharp shots, he describes aspects of ports, industry, architecture and new landscapes of wind turbines. But also burning topics like the circular economy are represented, among other things, by compressed waste. Seen from afar, the compressed waste packs turn into rolling dice. 
+
+Tom D'haenens will never disregard the role of man as scenarist of the great project of globalisation. Whether working in logistics or industry, time and again D'haenens approaches humans in a transformed reality. A labourer at work at a gigantic pipeline takes on surrealist allure. Through the eye of the camera reality becomes stronger and sometimes more bizarre. A plane turns into a gigantic albatross. Precisely because of this monumental and mesmerising vision of the photographer, the heavy industry and logistics sector call upon his great photographic talent. Especially in a world in full transition, like in the car industry, where new components such as batteries, sensors and robots are calling the shots. D'haenens metaphorically fixates these changes: sometimes static, sometimes cinematically dynamic.
+<br><br>
+Citizen of the world
+<br>
+Countless travels and tours have made D'haenens a citizen of the world. He braves oceans, deserts, mountain passes and deep valleys on missions that demand a lot of energy. D’haenens catalogues himself as a location photographer. His assignments come from all continents.
+
+The photographer is at his best when capturing industrial processes, airports, seaports and architecture. They are a snapshot of optimism and far-reaching innovation in various sectors. D'haenens frames his subjects with great precision and sharpness to reveal the true essence of reality. But also to depict the belief in a better future, translated into pure lines and structures with great aplomb. By playing with distance and cropping, many images take on the allure of an abstract painting. For instance, he metamorphoses an ordinary salt mountain into a mighty snowy landscape. The curvy shapes of a female nude flow seamlessly into the glowing sand plains of the Sahara. It has become his trademark to perceive reality differently, to create a contemporary world in which everything is rethought or reinvented.
+<br><br>
+Worldwide fame
+<br>
+Enchantment and candid enthusiasm are reflected in his images through never-before-seen viewpoints. A man welding seems to have ended up in a brutal roller coaster of a Bond film because of the cinematic light. It takes some effort from the viewer to thoroughly analyse the image. D'haenens forces the viewer to look and in doing so, he also exposes the ambiguity and essence of photography. "People only see what they already know and understand," Goethe wrote. Photographic images are normally subject to a powerful image theory, which makes it difficult for the viewer to see that there is not always an image. Great is the desire to see something recognizable.
+
+How exhausting is photography? According to D'haenens, there is always that eternal doubt, but that is precisely what keeps him sharp. It keeps him focused and alert. What D'haenens shows to the outside world is often what is called ‘le jamais vu’. Showing what the eye has never seen before. That is what makes D'haenens’ unique style, which has given him worldwide fame. The photographer as the modern Columbus who bundles the invisible contours of the Global World into a captivating anthology. Whether he believes in the 'decisive moment’? Is the photographer an all-seeing Zen Buddhist? Tom D'haenens puts things in perspective. In some cases, the moment counts, like the photograph showing hundreds of students listening to a speech. According to the photographer, by framing the picture differently, without a stage, you get an autonomous artistic print, the work and especially the photography stand on its own and the medium presents its enigmatic ambiguity. However, most images are not detached from the world. "I look at the world as it is," is his motto.
+<br><br>
+Living in America...
+<br>
+Photography is literally in D’haenens' genes. As a child he was fascinated by the medium and often borrowed his parents' camera. The seeds of a bright photographic future had been sown. From an early age, he was eager to discover the photographic equivalent of the world. That aspect has never left him until today; studio work, on the other hand, is not his cup of tea. "I need to feel and see the world," he says. 
+        </p>
+      </div>
+
+      <img src="logo.jpg" alt="logo View&Vision">
+      <p>
+      View&Vision Publishers specialiseert zich in het creëren en publiceren van hoogwaardige fotoboeken voor de industrie. Met een sterke focus op visuele storytelling brengt Viewvision de wereld van techniek, productie en industriële innovatie op een unieke en inspirerende manier in beeld.
+Door middel van krachtige fotografie en zorgvuldig vormgegeven publicaties documenteert Viewvision het vakmanschap, de mensen en de processen achter industriële bedrijven. Elk project wordt ontwikkeld met oog voor detail, kwaliteit en authenticiteit, waardoor de publicaties niet alleen informatief zijn, maar ook esthetisch aantrekkelijk.
+Viewvision Publishers werkt nauw samen met bedrijven om hun verhaal visueel te vertalen naar een tastbaar en duurzaam boek. Zo ontstaan publicaties die niet alleen het heden vastleggen, maar ook een waardevol document vormen voor de toekomst.
+Met een passie voor beeld, industrie en storytelling creëert Viewvision Publishers boeken die de kracht van industriële ondernemingen zichtbaar maken.
+      </p>
+
+      <p>
+      View&Vision Publishers specializes in creating and publishing high-quality photo books for the industrial sector. With a strong focus on visual storytelling, Viewvision captures the world of technology, production, and industrial innovation in a compelling and inspiring way.
+Through striking photography and carefully designed publications, Viewvision documents the craftsmanship, people, and processes behind industrial companies. Each project is developed with meticulous attention to detail, quality, and authenticity, making the books both informative and visually engaging.
+Working closely with businesses, Viewvision translates their stories into tangible, lasting publications that not only showcase the present but also serve as valuable records for the future.
+With a passion for imagery, industry, and storytelling, Viewvision Publishers creates books that reveal the power and creativity of industrial enterprises.
+      </p>
+
+      </div>`;
     };
   }
 
